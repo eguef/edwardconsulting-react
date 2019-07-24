fe3bba0e00dbb95cc574f82e99710de22cdc9244
eguonoefe <eguonoghene.efekemo@gmail.com>
2019-07-24 14:10:37 +0100

    Set up react application

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const app = document.getElementById('edward-consulting-app');
+
+const Test = () => (
+  <div>Testing that it works</div>
+)
+
+render(<Test />, app);
