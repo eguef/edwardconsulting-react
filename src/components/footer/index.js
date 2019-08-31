9fe62997114c90e775c6a7b879177543b667e1e1
eguonoefe <eguonoghene.efekemo@gmail.com>
2019-08-31 17:09:43 +0100

    Complete Page footer

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,7 +3,20 @@ import React from 'react';
 const Footer = () => (
   <div className="footer">
     <div className="footer-text">GET OUR NEWSLETTER TO STAY UP TO DATE</div>
-    <div className="footer-form" />
+    <div className="footer-form">
+      <div className="footer-form-input">
+        <input className="footer-input" type="text" placeholder="First Name" />
+      </div>
+      <div className="footer-form-input">
+        <input className="footer-input" type="text" placeholder="Last Name" />
+      </div>
+      <div className="footer-form-input">
+        <input className="footer-input" type="text" placeholder="Email" />
+      </div>
+      <div className="footer-form-input">
+        <button type="button" className="footer-button"><i className="fas fa-arrow-right" /></button>
+      </div>
+    </div>
     <div className="footer-others">
       <div className="footer-logo">
         <div className="footer-logo-image">
@@ -14,7 +27,13 @@ const Footer = () => (
           <div className="footer-logo-name-sub">Consulting</div>
         </div>
       </div>
-      <div className="footer-social" />
+      <div className="footer-social">
+        <div className="footer-contact-us">Contact us</div>
+        <i className="fab fa-facebook-square" />
+        <i className="fab fa-instagram" />
+        <i className="fab fa-twitter" />
+        <i className="fa fa-envelope" />
+      </div>
     </div>
   </div>
 );
